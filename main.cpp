f615b7ba804dc00fa795f19ffbce202902e53190
t1mn0 <timofeyalbecov@gmail.com>
2025-06-14 16:37:05 +0300

    Added a build as a header-only library via CMake (raw draft)

--- main.cpp
+++
@@ -1,8 +0,0 @@
-#include "include/Optional/Optional.hpp"
-#include "include/Result/Result.hpp"
-#include "include/Error/Error.hpp"
-
-int main() {
-    throwless::StringError e{};
-    auto t = throwless::Result<void, throwless::StringError>::Err(e);
-}
